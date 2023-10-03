cee887d44d7267277b732c4befde68a2efb9eab0
Tyran15 <mh15122006@gmail.com>
2023-10-03 16:11:47 -0300

    Adição do header e da barra de navegação

--- Navigator.js
+++ Navigator.js
@@ -1,18 +1,73 @@
 import React from 'react';
+import { Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import HomeScreen from './screens/HomeScreen';
+import Ficha from './screens/Ficha'
 import ConvMoedas from './screens/ConvMoedaParaDollar';
-const Stack = createStackNavigator();
+
+const Tab = createBottomTabNavigator();
 
 export default function Navigator() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Green Ephyr">
-        <Stack.Screen name="Green Ephyrz" component={HomeScreen} />
-        <Stack.Screen name="Convensor" component={ConvMoedas} />
-      </Stack.Navigator>
+      <Tab.Navigator
+        tabBarOptions={{
+          activeTintColor: 'white', // Cor das guias ativas
+          inactiveTintColor: '#efefef', // Cor das guias inativas
+          labelStyle: {
+            fontSize: 14,
+            fontWeight: 'bold',
+          },
+        }}
+        tabBarStyle={{
+          backgroundColor: '#9869F4', // Cor de fundo do tabNavigator
+        }}
+      >
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            tabBarLabel: 'Início',
+            tabBarIcon: ({ color, size }) => (
+              <Image
+                source={require('./Imagens/casa.png')}
+                style={{ width: size, height: size, tintColor: color }}
+              />
+            ),
+            tabBarStyle: {
+              backgroundColor: '#9869F4', // Cor de fundo específica para a tela HomeScreen
+            },
+          }}
+        />
+        <Tab.Screen
+          name="Ficha"
+          component={Ficha}
+          options={{
+            tabBarLabel: 'Ficha',
+            tabBarIcon: ({ color, size }) => (
+              <Image
+                source={require('./Imagens/ficha.png')}
+                style={{ width: size, height: size, tintColor: color }}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Configurações"
+          component={ConvMoedas}
+          options={{
+            tabBarLabel: 'Configurações',
+            tabBarIcon: ({ color, size }) => (
+              <Image
+                source={require('./Imagens/engrenagem.png')}
+                style={{ width: size, height: size, tintColor: color }}
+              />
+            ),
+          }}
+        />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,24 +1,84 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, Button, TouchableOpacity, Image } from 'react-native';
+import Carousel from 'react-native-snap-carousel';
+
+const data = [
+  { title: 'Câmbio de moedas', text: 'Câmbio das moedas do RPG', tela: 'Convensor' },
+  { title: 'Item 2', text: 'Conteúdo do Item 2' },
+  { title: 'Item 3', text: 'Conteúdo do Item 3' },
+];
 
 export default function HomeScreen({ navigation }) {
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      title: 'Home',
+      headerStyle: {
+        backgroundColor: '#9869F4', 
+      },
+      headerTintColor: 'white',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+    });
+  }, [navigation]);
+  
+  const renderItem = ({ item }) => (
+    <TouchableOpacity
+      style={styles.carouselItem}
+      onPress={() => navigation.navigate(item.tela)}
+    >
+      <View>
+        <Text style={styles.carouselText}>{item.title}</Text>
+        <Text style={styles.carouselText}>{item.text}</Text>
+        <Image />
+      </View>
+    </TouchableOpacity>
+  );
+
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Bem-vindo ao Green Ephyr</Text>
-      <Button
-        title="Convensor de moedas"
-        onPress={() => navigation.navigate('Convensor')}
+      <View>
+      </View>
+      <Carousel
+        data={data}
+        renderItem={renderItem}
+        sliderWidth={450}
+        sliderHeight={300}
+        itemWidth={280}
+        inactiveSlideScale={1}
+        inactiveSlideOpacity={1}
       />
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
     container: {
       flex: 1,
       alignItems: 'center',
+      marginTop: 50,
     },
-    text: {
-        fontSize: '1rem',
-    }
+    item: {
+      backgroundColor: 'lightgray',
+      borderRadius: 5,
+      padding: 20,
+      marginLeft: 25,
+      marginRight: 25,
+    },
+
+    carouselItem: {
+      backgroundColor: '#CB91F9',
+      padding: 25,
+      marginRight: 25,
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: 150,
+      borderRadius: 12,
+      width: 250,
+    },
+    carouselText: {
+      color: 'white'
+    },
   });
